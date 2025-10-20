fcde022122030d0c28370ba62905183e80e82d91
이정민 <ljm@ijeongmin-ui-MacBookPro.local>
2025-10-20 17:23:16 +0900

    [FEAT] Initial project setup

---
+++ src/main/java/com/zebra/ZebraApplication.java
@@ -0,0 +1,13 @@
+package com.zebra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZebraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZebraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zebra/ZebraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zebra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZebraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

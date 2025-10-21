2917772938ad726726c1d5a7a70600b66e28657d
youn <tty7797@naver.com>
2025-10-21 14:32:18 +0900

    feat : Brand 엔티티 & 리포지토리 생성

---
+++ src/main/java/com/zeebra/domain/brand/entity/Brand.java
@@ -0,0 +1,23 @@
+package com.zeebra.domain.brand.entity;
+
+import com.zeebra.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Brand extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String logoImage;
+}

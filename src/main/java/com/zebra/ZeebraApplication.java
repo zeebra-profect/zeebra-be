d4ff41f69d487f22a07efcde69fb824c1dd5d02c
youn <tty7797@naver.com>
2025-10-21 12:38:57 +0900

    style : application 이름 변경

--- src/main/java/com/zebra/ZeebraApplication.java
+++ src/main/java/com/zebra/ZeebraApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZebraApplication {
+public class ZeebraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZebraApplication.class, args);
+		SpringApplication.run(ZeebraApplication.class, args);
 	}
 
 }

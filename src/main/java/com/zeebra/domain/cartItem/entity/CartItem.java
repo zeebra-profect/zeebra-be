5ab18c876f4591abc0e5864ed77673ee463da73b
youn <tty7797@naver.com>
2025-10-21 14:21:58 +0900

    feat : CartItem 엔티티 & 리포지토리 생성

---
+++ src/main/java/com/zeebra/domain/cartItem/entity/CartItem.java
@@ -0,0 +1,29 @@
+package com.zeebra.domain.cartItem.entity;
+
+import com.zeebra.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class CartItem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long cartId;
+
+    private Long productOptionId;
+
+    private BigDecimal snapShotPrice;
+
+    private int quantity;
+}

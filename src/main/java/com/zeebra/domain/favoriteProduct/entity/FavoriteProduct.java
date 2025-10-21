bf5b28bc51d3cc8d2b3f3ae70a0beb1803dcd65d
youn <tty7797@naver.com>
2025-10-21 14:10:31 +0900

    feat : FavoriteProduct 엔티티 & 리포지토리 생성

---
+++ src/main/java/com/zeebra/domain/favoriteProduct/entity/FavoriteProduct.java
@@ -0,0 +1,27 @@
+package com.zeebra.domain.favoriteProduct.entity;
+
+import com.zeebra.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class FavoriteProduct extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long memberId;
+
+    private Long productId;
+
+    private LocalDateTime deletedAt;
+}

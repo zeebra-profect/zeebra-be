f4021984e48c2d1b84794910e498a2998a257e34
youn <tty7797@naver.com>
2025-10-21 14:34:28 +0900

    feat : Category 엔티티 & 리포지토리 생성

---
+++ src/main/java/com/zeebra/domain/category/entity/Category.java
@@ -0,0 +1,23 @@
+package com.zeebra.domain.category.entity;
+
+import com.zeebra.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Category extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long parentId;
+
+    private String name;
+}

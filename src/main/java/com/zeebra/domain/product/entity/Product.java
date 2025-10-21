923b992f79b0c3d31114762c0d35b8cd18380e3e
youn <tty7797@naver.com>
2025-10-21 13:45:46 +0900

    feat : Product 엔티티 & 리포지토리 생성

---
+++ src/main/java/com/zeebra/domain/product/entity/Product.java
@@ -0,0 +1,39 @@
+package com.zeebra.domain.product.entity;
+
+import com.zeebra.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long brandId;
+
+    private Long categoryId;
+
+    private String name;
+
+    private String description;
+
+    private String modelNumber;
+
+    private String thumbnail;
+
+    private List<String> images;
+
+    private int reviewCount;
+
+    private int favoriteProductCount;
+}

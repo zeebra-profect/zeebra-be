fa9b78b601af29574049bf1b96d05d677d958bc1
youn <tty7797@naver.com>
2025-10-21 13:57:34 +0900

    feat : OptionCombination 엔티티 & 리포지토리 생성

---
+++ src/main/java/com/zeebra/domain/optionCombination/entity/OptionCombination.java
@@ -0,0 +1,23 @@
+package com.zeebra.domain.optionCombination.entity;
+
+import com.zeebra.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class OptionCombination extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productOptionId;
+
+    private Long optionNameId;
+}

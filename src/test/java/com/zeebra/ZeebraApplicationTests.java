d8ddbc1d8d3bf7ce1adb5f6b0d456524a0865dbd
JungMinB7 <ljlm0829@gmail.com>
2025-10-21 14:52:16 +0900

    style: 패키지 이름 및 파일 이름 수정 ZBR-49

---
+++ src/test/java/com/zeebra/ZeebraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeebra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeebraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

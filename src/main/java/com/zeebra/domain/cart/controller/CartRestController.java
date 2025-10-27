a3d5929f94c724e96289e17605ab9fa542c82ca0
youn <tty7797@naver.com>
2025-10-27 11:21:48 +0900

    fix : Response API 적용 ZBR-92

--- src/main/java/com/zeebra/domain/cart/controller/CartRestController.java
+++ src/main/java/com/zeebra/domain/cart/controller/CartRestController.java
@@ -3,6 +3,7 @@ package com.zeebra.domain.cart.controller;
 import com.zeebra.domain.cart.dto.CartRequest;
 import com.zeebra.domain.cart.dto.CartResponse;
 import com.zeebra.domain.cart.service.CartService;
+import com.zeebra.global.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ public class CartRestController {
     private final CartService cartService;
 
     @PostMapping("/api/carts/{productOptionId}")
-    public CartResponse addCartItem(Long memberId, @PathVariable Long productOptionId, @RequestBody CartRequest request) {
+    public ApiResponse<CartResponse> addCartItem(Long memberId, @PathVariable Long productOptionId, @RequestBody CartRequest request) {
         return cartService.addCartItem(memberId, productOptionId, request);
     }
 }
--- src/main/java/com/zeebra/domain/product/service/ProductService.java
+++ src/main/java/com/zeebra/domain/product/service/ProductService.java
@@ -2,12 +2,13 @@ package com.zeebra.domain.product.service;
 
 import com.zeebra.domain.product.dto.FavoriteProductResponse;
 import com.zeebra.domain.product.dto.ProductDetailResponse;
+import com.zeebra.global.ApiResponse;
 
 public interface ProductService {
 
-    public ProductDetailResponse getProductDetail(Long productId);
+    public ApiResponse<ProductDetailResponse> getProductDetail(Long productId);
 
-    public FavoriteProductResponse addFavoriteProduct(Long memberId, Long productId);
+    public ApiResponse<FavoriteProductResponse> addFavoriteProduct(Long memberId, Long productId);
 
-    public void deleteFavoriteProduct(Long memberId, Long productId);
+    public ApiResponse<Void> deleteFavoriteProduct(Long memberId, Long productId);
 }

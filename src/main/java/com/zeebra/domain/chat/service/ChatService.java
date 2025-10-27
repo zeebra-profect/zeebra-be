19c7f26d2b5bb63b06e529b6a7075d7c1d521334
JungMinB7 <ljlm0829@gmail.com>
2025-10-27 10:23:25 +0900

    style: ChatService 주석처리 해제 ZBR-87

--- src/main/java/com/zeebra/domain/chat/service/ChatService.java
+++ src/main/java/com/zeebra/domain/chat/service/ChatService.java
@@ -14,23 +14,31 @@ public interface ChatService {
      */
     ChatRoomResponseDto createOrGetChatRoom(ChatRoomRequestDto chatRoomRequestDto, Long currentMemberId);
 
-//    /**
-//     * 실시간 채팅 메세지 저장
-//     * @param chatMessageRequestDto
-//     * @param currentMemberId
-//     * @return 저장된 메세지
-//     */
-//    @Transactional
-//    ChatMessageResponseDto saveMessage(ChatMessageRequestDto chatMessageRequestDto, Long currentMemberId);
-//
-//    /**
-//     * 채팅방 메세지 조회
-//     */
-//    Page<ChatMessageResponseDto> getChatHistory(Long chatRoomId, Pageable pageable);
-//
-//    /**
-//     * 거래 제안
-//     */
-//    @Transactional
-//    TradeResponseDto proposeTrade(Long chatRoomId, TradeRequestDto tradeRequestDto, Long currentMemberId);
+    /**
+     * 실시간 채팅 메세지 저장
+     * @param chatMessageRequestDto
+     * @param currentMemberId
+     * @return 저장된 메세지
+     */
+    @Transactional
+    ChatMessageResponseDto saveMessage(ChatMessageRequestDto chatMessageRequestDto, Long currentMemberId);
+
+    /**
+     * 채팅방 메세지 조회
+     */
+    Page<ChatMessageResponseDto> getChatHistory(Long chatRoomId, Pageable pageable);
+
+    /**
+     * 채팅방 나가기
+     * @param chatRoomId 나갈 채팅방의 ID
+     * @param currentMemberId 나가는 사용자의 ID
+     */
+    void leaveChatRoom(Long chatRoomId, Long currentMemberId);
+
+
+    /**
+     * 거래 제안
+     */
+    @Transactional
+    TradeResponseDto proposeTrade(Long chatRoomId, TradeRequestDto tradeRequestDto, Long currentMemberId);
 }

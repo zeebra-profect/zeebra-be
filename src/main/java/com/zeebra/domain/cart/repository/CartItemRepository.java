104a7e964cdaeb7e69b7eed2f223a566d714fb53
gayoung127 <dinin1217@gmail.com>
2025-10-29 08:43:20 +0900

    feat: CartItem 조회 메서드 추가
    - Cart ID로 CartItem 리스트를 조회하는 메서드 추가 (`findByCartId`)
    
    ZBR-88

--- src/main/java/com/zeebra/domain/cart/repository/CartItemRepository.java
+++ src/main/java/com/zeebra/domain/cart/repository/CartItemRepository.java
@@ -1,7 +1,12 @@
 package com.zeebra.domain.cart.repository;
 
-import com.zeebra.domain.cart.entity.CartItem;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.zeebra.domain.cart.entity.CartItem;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+	List<CartItem> findByCartId(Long cartId);
 }

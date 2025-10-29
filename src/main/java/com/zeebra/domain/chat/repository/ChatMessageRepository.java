d30743a9185957b724c131588b4e3f8c1c1d79b9
JungMinB7 <ljlm0829@gmail.com>
2025-10-29 15:20:50 +0900

    feat: 채팅방 목록 조회 쿼리 메소드 추가 ZBR-120

--- src/main/java/com/zeebra/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/zeebra/domain/chat/repository/ChatMessageRepository.java
@@ -5,7 +5,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
     Page<ChatMessage> findByChatRoomMemberChatRoomId(Long chatRoomId, Pageable pageable);
+
+    Optional<ChatMessage> findById(Long lastMessageId);
+
+    long countByChatRoomMember_ChatRoomIdAndIdGreaterThan(Long chatRoomId, Long lastReadMessageId);
+
+    Optional<ChatMessage> findFirstByChatRoomMember_ChatRoomIdOrderByIdDesc(Long chatRoomId);
 }

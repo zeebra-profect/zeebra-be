190f6d81fb2357251278606fbfaae88cf1f35952
JungMinB7 <ljlm0829@gmail.com>
2025-11-18 15:32:13 +0900

    JpaAuditing 중복 제거 ZBR-175

--- src/test/java/com/zeebra/ZeebraApplicationTests.java
+++ src/test/java/com/zeebra/ZeebraApplicationTests.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
-//@SpringBootApplication
 public class ZeebraApplicationTests {
 
     public static void main(String[] args) {

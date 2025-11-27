d9db5db8a976c53acb144b76c4413520cdf883dd
JungMinB7 <ljlm0829@gmail.com>
2025-11-27 10:47:29 +0900

    모니터링 관련 파일 수정 ZBR-196

--- src/main/java/com/zeebra/domain/chat/controller/ChatSocketController.java
+++ src/main/java/com/zeebra/domain/chat/controller/ChatSocketController.java
@@ -26,25 +26,9 @@ public class ChatSocketController {
 
     private final ChatService chatService;
     private final SimpMessagingTemplate messagingTemplate;
+
     private final MeterRegistry meterRegistry;
 
-    private Counter messageCounter;
-    private Timer messageTimer;
-
-    @PostConstruct
-    void initMetrics() {
-        // 총 메시지 수
-        this.messageCounter = Counter.builder("ws_chat_messages_total")
-                .description("Total number of WebSocket chat messages processed")
-                .tag("endpoint", "/chat/message")  // 나중에 endpoint별로 볼 수 있게
-                .register(meterRegistry);
-
-        // 메시지 처리 시간
-        this.messageTimer = Timer.builder("ws_chat_message_seconds")
-                .description("Time taken to process WebSocket chat messages")
-                .tag("endpoint", "/chat/message")
-                .register(meterRegistry);
-    }
 
     @MessageMapping("/chat/message")
     public void sendMessage(
@@ -77,14 +61,19 @@ public class ChatSocketController {
                     savedMessage
             );
             log.info("[WebSocket] 메시지 전송 성공: (Room: {})", savedMessage.roomId());
-            messageCounter.increment(); // 메트릭 증가
+
+            Counter counter = meterRegistry.counter("chat-message-total", "endpoint", "/chat/message");
+
+            counter.increment(); // 메트릭 증가
 
         } catch (Exception e) {
             //  3. 에러 발생 시 서버 로그(터미널)에 에러 메시지 출력
             log.error("Failed to send WebSocket message: {}", e.getMessage());
             e.printStackTrace(); // (더 자세한 스택 트레이스)
         } finally {
-            sample.stop(messageTimer); // 처리 시간 기록
+            Timer timer = meterRegistry.timer("ws_chat_message_seconds", "endpoint", "/chat/message");
+
+            sample.stop(timer); // 처리 시간 기록
         }
     }
 }

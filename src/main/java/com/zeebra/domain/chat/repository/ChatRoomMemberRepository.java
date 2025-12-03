7e4249a6b29640495505dd0669d1a6d93c8d18c7
JungMinB7 <ljlm0829@gmail.com>
2025-12-03 11:04:45 +0900

    feat: chatRoomMember 쿼리dsl ZBR-215

--- src/main/java/com/zeebra/domain/chat/repository/ChatRoomMemberRepository.java
+++ src/main/java/com/zeebra/domain/chat/repository/ChatRoomMemberRepository.java
@@ -2,8 +2,12 @@ package com.zeebra.domain.chat.repository;
 
 import com.zeebra.domain.chat.entity.ChatRoom;
 import com.zeebra.domain.chat.entity.ChatRoomMember;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.inject.Qualifier;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +15,15 @@ import java.util.Optional;
 public interface ChatRoomMemberRepository extends JpaRepository<ChatRoomMember, Long> {
 
     Optional<ChatRoomMember> findByChatRoomIdAndMemberId(Long chatRoomId, Long memberId);
-    List<ChatRoomMember> findByMemberIdAndDeletedAtIsNull(Long memberId);
 
     List<ChatRoomMember> findByChatRoomIdAndDeletedAtIsNull(Long chatRoomId);
+
+    @EntityGraph(attributePaths = {"chatRoom"})
+    List<ChatRoomMember> findByMemberIdAndDeletedAtIsNull(Long memberId);
+
+    // 여러 채팅방의 상대방 정보들 한 번에 조회 -> 내가 속한 방들 나 제외한 모든 유저 가져옴
+    @Query("SELECT crm FROM ChatRoomMember crm JOIN FETCH crm.member " +
+            "WHERE crm.chatRoom.id IN :roomIds AND crm.memberId <> :myMemberId " +
+            "AND crm.deletedAt IS NULL")
+    List<ChatRoomMember> findAllOpponents(@Param("roomIds") List<Long> roomIds, @Param("myMemberId") Long myMemberId);
 }
